4b83be9e14cc93d0c0417e986647119646ed157f
Sarveshwar Mahapatro <f20190257@goa.bits-pilani.ac.in>
2022-08-26 15:41:52 +0530

    Products added

--- client/src/components/home/mainComponent.js
+++ client/src/components/home/mainComponent.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Banner from './Banner'
 import './home.css'
-
+import Slide from './Slide'
 const mainComponent = () => {
   return (
     <div className='home_section'>
         <div className="banner_part">
             <Banner />
         </div>
-      
+        <Slide />
     </div>
   )
 }
