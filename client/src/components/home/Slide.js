ca40589793615f1ec26357d79814ad64415a2fec
Sarveshwar Mahapatro <f20190257@goa.bits-pilani.ac.in>
2022-08-27 02:17:34 +0530

    Client-side completed

--- client/src/components/home/Slide.js
+++ client/src/components/home/Slide.js
@@ -33,7 +33,7 @@ const Slide = ({title}) => {
             <button className='view_btn'>View All</button>
         </div>
         <Divider />
-        <Carousel responsive={responsive} infinite={true} draggable={false} swipeable={true} showDots={false} centerMode={true} autoPlay={true} autoPlaySpeed={1000} keyBoardControl={true} removeArrowOnDeviceType={["tablet", "mobile"]} dotListClass="custom-dot-list-style" itemClass="carousel-item-padding-40-px" containerClass="carousel-container">
+        <Carousel responsive={responsive} infinite={true} draggable={false} swipeable={true} showDots={false} centerMode={true} autoPlay={true} autoPlaySpeed={1500} keyBoardControl={true} removeArrowOnDeviceType={["tablet", "mobile"]} dotListClass="custom-dot-list-style" itemClass="carousel-item-padding-40-px" containerClass="carousel-container">
             {
                 products.map((e)=>{
                     return (
