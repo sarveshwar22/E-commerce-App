0b72b7510a670bb4476a914db0e2852968ba7b20
Sarveshwar Mahapatro <f20190257@goa.bits-pilani.ac.in>
2022-08-27 10:42:24 +0530

    Database added

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose=require("mongoose")
+require('./db/connection');
 
+const Products = require('./models/productsSchema');
+
+const DefaultData = require('./defaultData')
 
 const port = 8005;
 app.listen(port,()=>{
     console.log(`console is running on port number ${port}`);
 })
+
+DefaultData();
