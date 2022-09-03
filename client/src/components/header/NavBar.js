2c945d3cf0c4cbe204f36c2def2d08e1d3ee3dc4
Sarveshwar Mahapatro <f20190257@goa.bits-pilani.ac.in>
2022-09-03 18:16:34 +0530

    Sidebar added

--- client/src/components/header/NavBar.js
+++ client/src/components/header/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './NavBar.css'
 import SearchIcon from '@mui/icons-material/Search';
 import Avatar from '@mui/material/Avatar';
@@ -7,9 +7,19 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { NavLink } from 'react-router-dom';
 import { LoginContext } from '../context/ContextProvider';
 
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+import Drawer from '@mui/material/Drawer';
+import Rightheader from './Rightheader';
+
+
+
 const NavBar = () => {
 
   const {account,setAccount} = useContext(LoginContext);
+  
+  const [dropen,setDropen] = useState(false);
   console.log(account);
   console.log("SUCESSSSS");
   
@@ -32,6 +42,16 @@ const NavBar = () => {
         setAccount(data);
     }
 }
+
+const handleopen= ()=>{
+    setDropen(true);
+}
+
+const handledrclose = ()=>{
+    setDropen(false);
+
+}
+
 useEffect(() => {
     getdetailsvaliduser()
 }, []);
@@ -40,6 +60,15 @@ useEffect(() => {
     <header>
         <nav>
             <div className='left'>
+            
+            <IconButton className='hamburger' onClick={handleopen}>
+            <MenuIcon style={{color:"white"}}/>
+          </IconButton>
+
+            <Drawer open={dropen} onClose={handledrclose}>
+                <Rightheader logclose={handledrclose}/>
+            </Drawer>
+            
                 <div className='navlogo'>
                     {/* <img src='./logo_main.png' alt='TechZen' /> */}
                     <NavLink to="/" >
